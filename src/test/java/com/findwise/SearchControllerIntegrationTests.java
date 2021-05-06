7367accd00fdc86fddfc4d7d5b79f5fe0a9e2032
Adam Gierek <gierek.adam93@gmail.com>
2021-05-06 12:44:28 +0200

    Add integration test for POST request

--- src/test/java/com/findwise/SearchControllerIntegrationTests.java
+++ src/test/java/com/findwise/SearchControllerIntegrationTests.java
@@ -1,18 +1,28 @@
 package com.findwise;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.jfr.ContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.RestDocsRestAssuredConfigurationCustomizer;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class SearchControllerIntegrationTests {
+
    @Autowired
    private MockMvc mockMvc;
    @Autowired
@@ -36,4 +46,17 @@ public class SearchControllerIntegrationTests {
          e.printStackTrace();
       }
    }
+   @Test
+   void httpPost_AddDocuments(){
+      //given & when
+      try {
+         mockMvc.perform(MockMvcRequestBuilders.post("/documents")
+                 .content("brown fox jumped over the brown dog")
+                 .contentType(MediaType.APPLICATION_JSON)).andDo(print());
+
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+   }
+
 }
